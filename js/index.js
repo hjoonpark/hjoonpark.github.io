3e56b59faa973bafa03d289e4c01c4dd9c2ed530
Joon Park <hjoonpark.us@gmail.com>
2021-03-23 17:41:39 -0600

    firebase firestore modified ip ajax

--- js/index.js
+++ js/index.js
@@ -3,15 +3,9 @@ $(document).ready(function(){
 });
 
 function ipLookUp (handleData) {
-    $.ajax('http://ip-api.com/json')
-    .then(
-        function success(res) {
-            handleData(res);
-        },
-
-        function fail(data, status) {
-        }
-    );
+    $.get("https://api.ipdata.co?api-key=test", function(response) {
+        handleData(response);
+    }, "jsonp");
 }
 
 var firestore = firebase.firestore();
@@ -20,20 +14,28 @@ const collection = firestore.collection("users");
 window.onload = function() {
     ipLookUp(function(d) {
         collection.add({
-            "as": d.as,
+            "asn_name": d.asn.name,
+            "asn_domain": d.asn.domain,
+            "current_time": d.time_zone.current_time,
+            "country_code": d.country_code,
+            "country_name": d.country_name,
             "city": d.city,
-            "country": d.country,
-            "countryCode": d.countryCode,
-            "isp": d.isp,
-            "lat": d.lat,
-            "lon": d.lon,
-            "org": d.org,
-            "query": d.query,
             "region": d.region,
-            "regionName": d.regionName,
-            "status": d.status,
-            "timezone": d.timezone,
-            "zip": d.zip
+            "region_code": d.region_code,
+            "postal": d.postal,
+            "ip": d.ip,
+            "language_name": d.languages[0].name,
+            "language_native": d.languages[0].native,
+            "latitude": d.latitude,
+            "longitude": d.longitude,
+            "time_zone": d.time_zone.name,
+            "z_threat_anonymous": d.threat.is_anonymous,
+            "z_threat_bogon": d.threat.is_bogon,
+            "z_threat_known_abuser": d.threat.is_known_abuser,
+            "z_threat_known_attacker": d.threat.is_known_attacker,
+            "z_threat_proxy": d.threat.is_proxy,
+            "z_threat_threat": d.threat.is_threat,
+            "z_threat_tor": d.threat.is_tor,
         }).then(function() {
             console.log("Welcome!");
         })
--- tracker.php
+++
@@ -1,4 +0,0 @@
-<?php
-$txt = "PHP";
-echo "I love $txt!";
-?>
